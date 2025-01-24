01ab0cd8f534ca5409818ce21f17b9c016559e79
awais3344 <awaisalishah02@gmail.com>
2025-01-24 19:19:41 +0500

    serializers added

---
+++ tasks/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+    def validate_due_date(self, value):
+        if value < timezone.now().date():
+            raise serializers.ValidationError("Due date cannot be in the past.")
+        return value
